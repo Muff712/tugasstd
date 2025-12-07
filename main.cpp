3973e9fd9ffce793fa868a498f6f000ce0205a81
Afiqah Nazifatul Latif <afiqahnazifatull@gmail.com>
2025-12-07 22:07:38 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "Tugas_Besar.h"
+#include "Tugas Besar.h"
 
 using namespace std;
 
 int main(){
     List library;      // Library untuk semua lagu
     List playlist;     // Playlist user
-    List favorite;     // Playlist favorit user (blom diupdate)
+    List favorite;     // Playlist favorit user
     address currentSong = nullptr;  // Untuk menyimpan lagu
     infotype lagu;
     string pilihan;
@@ -21,16 +21,15 @@ int main(){
         cout << endl;
 
         if (pilihan == "Admin" || pilihan == "admin" || pilihan == "1") {
-            int maunyaadmin = -1; // ini yang buat biar enggak langsung keluar dari menu admin
+            int maunyaadmin = -1;
 
-            while(maunyaadmin != 0){
-                cout << "\n=== MENU ADMIN ===" << endl;
+            while (maunyaadmin != 0){
+                cout << "\n========== MENU ADMIN ==========" << endl;
                 cout << "1. Menambahkan lagu ke library" << endl;
                 cout << "2. Melihat semua lagu di library" << endl;
                 cout << "3. Mengubah data lagu di library" << endl;
                 cout << "4. Menghapus data lagu di library" << endl;
                 cout << "0. Kembali ke menu utama" << endl;
-                cout << "10. keluar apk";
                 cout << "Pilih: ";
                 cin >> maunyaadmin;
 
@@ -44,19 +43,19 @@ int main(){
                 } else if (maunyaadmin == 4) {
                     deleteSong(library);
                 } else if (maunyaadmin == 0) {
-                    cout << "Kembali ke menu utama.\n";
                 } else {
-                    cout << "terimkasih telah menggunakan apk enggak jelas ini.\n";
-                    return 0;
+                    cout << "Pilihan tidak valid!\n";
                 }
             }
         }
         else if (pilihan == "User" || pilihan == "user" || pilihan == "2") {
             int maunyauser = -1;
+            address song = nullptr;
+            List daftarlagu;
 
             while(maunyauser != 0){
-                cout << "\n=== MENU USER ===" << endl;
-                cout << "1. Mencari lagu berdasarkan ID" << endl;
+                cout << "\n========= MENU USER =========" << endl;
+                cout << "1. Mencari lagu" << endl;
                 cout << "2. Memutar lagu dari library" << endl;
                 cout << "3. Menghentikan lagu" << endl;
                 cout << "4. Memutar lagu selanjutnya" << endl;
@@ -65,48 +64,113 @@ int main(){
                 cout << "7. Menghapus lagu dari playlist" << endl;
                 cout << "8. Melihat isi playlist" << endl;
                 cout << "0. Kembali ke menu utama" << endl;
-                cout << "10. keluar apk";
                 cout << "Pilih: ";
                 cin >> maunyauser;
 
                 cout << endl;
                 if (maunyauser == 1) {
-                    userSearchSong(library);
-                } else if (maunyauser == 2) {
-                    int pilihan;
-                    cout << "Cari lagu berdasarkan:\n";
-                    cout << "1. Judul Lagu\n";
-                    cout << "2. ID Lagu\n";
-                    cout << "Pilih: ";
+                    string pilihan;
+                    cout << "1. ID" << endl;
+                    cout << "2. Judul" << endl;
+                    cout << "3. Penyanyi" << endl;
+                    cout << "Cari lagu berdasarkan: ";
                     cin >> pilihan;
 
-                    if(pilihan == 1){
+                    if (pilihan == "ID" || pilihan == "id" || pilihan == "1") {
+                        string id;
+
+                        cout << "Masukkan ID lagu: ";
+                        cin >> id;
+                        song = searchById(library, id);
+
+                        if (song != nullptr) {
+                            cout << "Lagu ditemukan!\n";
+                            showSong(song);
+                        } else {
+                            cout << "Lagu tidak ditemukan!\n";
+                        }
+                    }
+                    if (pilihan == "Judul" || pilihan == "judul" || pilihan == "2") {
+                        string judul;
+
+                        cout << "Masukkan judul lagu: ";
+                        cin >> judul;
+                        song = searchByTitle(library, judul);
+
+                        if (song != nullptr) {
+                            cout << "Lagu ditemukan!\n";
+                            showSong(song);
+                        } else {
+                            cout << "Lagu tidak ditemukan!\n";
+                        }
+                    }
+                    if (pilihan == "Penyanyi" || pilihan == "penyanyi" || pilihan == "3") {
+                        string penyanyi;
+
+                        cout << "Masukkan penyanyi: ";
+                        cin >> penyanyi;
+                        daftarlagu = searchBySinger(library, penyanyi);
+
+                        if (!isEmpty(daftarlagu)) {
+                            cout << "Lagu ditemukan!\n";
+                            showAllSongs(daftarlagu);
+                        } else {
+                            cout << "Lagu tidak ditemukan!\n";
+                        }
+                    }
+                } else if (maunyauser == 2) {
+                    string pilihan;
+                    cout << "1. ID" << endl;
+                    cout << "2. Judul" << endl;
+                    cout << "3. Penyanyi" << endl;
+                    cout << "Cari lagu berdasarkan: ";
+                    cin >> pilihan;
+
+                    if (pilihan == "ID" || pilihan == "id" || pilihan == "1"){
+                        string id;
+
+                        cout << "Masukkan ID lagu: ";
+                        cin >> id;
+                        song = searchById(library, id);
+
+                        if (song != nullptr) {
+                            currentSong = song;
+                            playSong(currentSong);
+                        } else {
+                            cout << "Lagu tidak ditemukan!\n";
+                        }
+                    } else if (pilihan == "Judul" || pilihan == "judul" || pilihan == "2"){
                         string judul;
                         cout << "Masukkan judul lagu: ";
                         cin >> judul;
+                        song = searchByTitle(library, judul);
 
-                        address temp = library.first;
-                        bool found = false;
-                        while(temp != nullptr && !found){
-                            if(temp->info.judul == judul){
-                                currentSong = temp;
-                                playSong(currentSong);
-                                found = true;
-                            }
-                            temp = temp->next;
-                        }
-                        if(!found){
-                            cout << "Lagu tidak ditemukan.\n";
-                        }
-                    } else if(pilihan == 2){
-                        string id;
-                        cout << "Masukkan ID lagu: ";
-                        cin >> id;
-                        currentSong = findSong(library, id);
-                        if(currentSong != nullptr){
+                        if (song != nullptr){
+                            currentSong = song;
                             playSong(currentSong);
                         } else {
-                            cout << "Lagu tidak ditemukan.\n";
+                            cout << "Lagu tidak ditemukan!\n";
+                        }
+                    } else if (pilihan == "Penyanyi" || pilihan == "penyanyi" || pilihan == "3") {
+                        string penyanyi;
+                        cout << "Masukkan penyanyi: ";
+                        cin >> penyanyi;
+                        List daftarlagu = searchBySinger(library, penyanyi);
+
+                        if (isEmpty(daftarlagu)) {
+                            cout << "Tidak ada lagu dari penyanyi tersebut.\n";
+                        } else {
+                            cout << "Daftar lagu oleh " << penyanyi << ": \n";
+                            showAllSongs(daftarlagu);
+
+                            string id;
+
+                            cout << "Masukkan ID lagu: ";
+                            cin >> id;
+                            song = searchById(library, id);
+
+                            currentSong = song;
+                            playSong(currentSong);
                         }
                     } else {
                         cout << "Pilihan tidak valid.\n";
@@ -118,63 +182,68 @@ int main(){
                 } else if (maunyauser == 5) {
                     prevSong(currentSong);
                 } else if (maunyauser == 6) {
-                    int pilihan;
-                    cout << "Cari lagu yang ingin ditambahkan ke playlist berdasarkan:\n";
-                    cout << "1. Judul Lagu\n";
-                    cout << "2. ID Lagu\n";
-                    cout << "Pilih: ";
+                    string pilihan;
+                    cout << "1. ID" << endl;
+                    cout << "2. Judul" << endl;
+                    cout << "3. Penyanyi" << endl;
+                    cout << "Cari lagu berdasarkan: ";
                     cin >> pilihan;
 
                     address song = nullptr;
 
-                    if(pilihan == 1){
-                        string judul;
-                        cout << "Masukkan judul lagu: ";
-                        cin >> judul;
-
-                        address temp = library.first;
-                        while(temp != nullptr && song == nullptr){
-                            if(temp->info.judul == judul){
-                                song = temp;
-                            }
-                            temp = temp->next;
-                        }
-                    } else if(pilihan == 2){
+                    if (pilihan == "ID" || pilihan == "id" || pilihan == "1") {
                         string id;
+
                         cout << "Masukkan ID lagu: ";
                         cin >> id;
-                        song = findSong(library, id);
-                    } else {
-                        cout << "Pilihan tidak valid.\n";
-                    }
+                        song = searchById(library, id);
+                    } else if (pilihan == "Judul" || pilihan == "judul" || pilihan == "2") {
+                        string judul;
 
-                    if(song != nullptr){
+                        cout << "Masukkan judul lagu: ";
+                        cin >> judul;
+                        song = searchByTitle(library, judul);
+                    } else if (pilihan == "Penyanyi" || pilihan == "penyanyi" || pilihan == "3") {
+                        string penyanyi;
+                        cout << "Masukkan penyanyi: ";
+                        cin >> penyanyi;
+                        List daftarlagu = searchBySinger(library, penyanyi);
+
+                        if (isEmpty(daftarlagu)) {
+                            cout << "Tidak ada lagu dari penyanyi tersebut.\n";
+                        } else {
+                            cout << "Daftar lagu oleh " << penyanyi << ":\n";
+                            showAllSongs(daftarlagu);
+
+                            string id;
+
+                            cout << "Masukkan ID lagu: ";
+                            cin >> id;
+                            song = searchById(library, id);
+                        }
+                    } else {
+                        cout << "Pilihan tidak valid!\n";
+                    }
+                    if (song != nullptr){
                         addToPlaylist(playlist, song);
                     } else {
-                        cout << "Lagu tidak ditemukan di library.\n";
+                        cout << "Lagu tidak ditemukan!\n";
                     }
                 } else if (maunyauser == 7) {
                     removeFromPlaylist(playlist);
                 } else if (maunyauser == 8) {
                     showPlaylist(playlist);
                 } else if (maunyauser == 0) {
-                    cout << "Kembali ke menu utama.\n";
                 } else {
                     cout << "Pilihan tidak valid!\n";
                 }
             }
         }
-        else if (pilihan == "0" || pilihan == "keluar" || pilihan == "Keluar") {
+        else if (pilihan == "Keluar" || pilihan == "keluar" || pilihan == "0") {
             cout << "Terima kasih telah menggunakan aplikasi!\n";
-            return 0;
-        }
-        else {
-            cout << "terimkasih telah menggunakan apk enggak jelas ini.\n";
             return 0;
         }
     }
 
     return 0;
 }
-
-// fitur keluar disatuin
