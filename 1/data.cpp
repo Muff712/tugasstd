b04f894cfbba165a4b6b4f50c43a893a69bff5c7
Muflih Rafif Ibrahim <muflihrafifibrahim712@gmail.com>
2025-12-14 18:35:04 +0700

    nambah lagu

--- 1/data.cpp
+++ 1/data.cpp
@@ -3,66 +3,126 @@
 void initDummySongs(List &L) {
     infotype x;
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "001";
+    x.judul = "Gurenge";
+    x.penyanyi = "LiSA";
+    x.durasi = "4:02";
     insertLast(L, createElement(x));
 
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "002";
+    x.judul = "Homura";
+    x.penyanyi = "LiSA";
+    x.durasi = "4:17";
     insertLast(L, createElement(x));
 
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "003";
+    x.judul = "Yoru ni Kakeru";
+    x.penyanyi = "YOASOBI";
+    x.durasi = "4:22";
     insertLast(L, createElement(x));
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "004";
+    x.judul = "Idol";
+    x.penyanyi = "YOASOBI";
+    x.durasi = "3:32";
     insertLast(L, createElement(x));
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "005";
+    x.judul = "Lemon";
+    x.penyanyi = "Kenshi Yonezu";
+    x.durasi = "4:15";
     insertLast(L, createElement(x));
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "006";
+    x.judul = "Resister";
+    x.penyanyi = "ASCA";
+    x.durasi = "4:42";
     insertLast(L, createElement(x));
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "007";
+    x.judul = "Catch the Moment";
+    x.penyanyi = "LiSA";
+    x.durasi = "4:45";
     insertLast(L, createElement(x));
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "008";
+    x.judul = "Haikei Goodbye Sayonara";
+    x.penyanyi = "ASCA";
+    x.durasi = "4:28";
     insertLast(L, createElement(x));
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "009";
+    x.judul = "Kaibutsu";
+    x.penyanyi = "YOASOBI";
+    x.durasi = "3:29";
     insertLast(L, createElement(x));
 
-    x.id = "";
-    x.judul = "";
-    x.penyanyi = "";
-    x.durasi = "";
+    x.id = "010";
+    x.judul = "Paprika";
+    x.penyanyi = "Kenshi Yonezu";
+    x.durasi = "4:12";
+    insertLast(L, createElement(x));
+
+    x.id = "011";
+    x.judul = "Akeboshi";
+    x.penyanyi = "LiSA";
+    x.durasi = "4:33";
+    insertLast(L, createElement(x));
+
+    x.id = "012";
+    x.judul = "Shiawase";
+    x.penyanyi = "YOASOBI";
+    x.durasi = "4:08";
+    insertLast(L, createElement(x));
+
+    x.id = "013";
+    x.judul = "Loser";
+    x.penyanyi = "Kenshi Yonezu";
+    x.durasi = "4:29";
+    insertLast(L, createElement(x));
+
+    x.id = "014";
+    x.judul = "PLEDGE";
+    x.penyanyi = "LiSA";
+    x.durasi = "4:18";
+    insertLast(L, createElement(x));
+
+    x.id = "015";
+    x.judul = "Howling";
+    x.penyanyi = "ASCA";
+    x.durasi = "4:15";
+    insertLast(L, createElement(x));
+
+    x.id = "016";
+    x.judul = "Gunjou";
+    x.penyanyi = "YOASOBI";
+    x.durasi = "3:57";
+    insertLast(L, createElement(x));
+
+    x.id = "017";
+    x.judul = "Peace Sign";
+    x.penyanyi = "Kenshi Yonezu";
+    x.durasi = "4:01";
+    insertLast(L, createElement(x));
+
+    x.id = "018";
+    x.judul = "Rising Hope";
+    x.penyanyi = "LiSA";
+    x.durasi = "4:24";
+    insertLast(L, createElement(x));
+
+    x.id = "019";
+    x.judul = "CHAIN";
+    x.penyanyi = "ASCA";
+    x.durasi = "4:36";
+    insertLast(L, createElement(x));
+
+    x.id = "020";
+    x.judul = "Moshimo Inochi ga Egaketara";
+    x.penyanyi = "YOASOBI";
+    x.durasi = "3:44";
     insertLast(L, createElement(x));
 
 }
